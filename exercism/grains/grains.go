ac41cd47511fe575f5fb41f6fabdd38bc1b20fdf
Dmitri Y. Lapshin <spacehug.o0@gmail.com>
2019-04-10 18:06:49 +0500

    ⚗️ Grains

---
+++ exercism/grains/grains.go
@@ -0,0 +1,17 @@
+package grains
+
+import (
+	"errors"
+)
+
+func Total() uint64 {
+	return (1 << 64) - 1
+}
+
+func Square(n int) (uint64, error) {
+	if n < 1 || n > 64 {
+		return 0, errors.New("integer out of bounds")
+
+	}
+	return 1 << uint64(n - 1), nil
+}
