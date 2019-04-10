00dbdd834d9d402fdeed54b5e77136f99fa698b9
Dmitri Y. Lapshin <spacehug.o0@gmail.com>
2019-04-10 17:41:58 +0500

    ⚗️  Isogram

---
+++ exercism/isogram/isogram.go
@@ -0,0 +1,16 @@
+package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+func IsIsogram(s string) bool {
+	s = strings.ToLower(s)
+	for i, r := range s {
+		if unicode.IsLetter(r) && strings.ContainsRune(s[i + 1:], r) {
+			return false
+		}
+	}
+	return true
+}
