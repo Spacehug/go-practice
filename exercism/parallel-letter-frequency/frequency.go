41fcc11c7c3d4a098d2af9c48e8210a7ec7c5819
Dmitri Y. Lapshin <spacehug.o0@gmail.com>
2019-04-11 11:26:27 +0500

    ⚗️ Parallel letter frequency

---
+++ exercism/parallel-letter-frequency/frequency.go
@@ -0,0 +1,27 @@
+package letter
+
+type FreqMap map[rune]int
+
+func Frequency(s string) FreqMap {
+	m := FreqMap{}
+	for _, r := range s {
+		m[r]++
+	}
+	return m
+}
+
+func ConcurrentFrequency(sl []string) FreqMap {
+	op := FreqMap{}
+	c := make(chan FreqMap)
+	for _, s := range sl {
+		go func(s string) {
+			c <- Frequency(s)
+		}(s)
+	}
+	for range sl {
+		for k, v := range <- c {
+			op[k] += v
+		}
+	}
+	return op
+}
