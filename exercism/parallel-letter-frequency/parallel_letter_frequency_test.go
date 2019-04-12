459487a44441530982ffd1909e6fc08d084d0c0f
Dmitri Y. Lapshin <spacehug.o0@gmail.com>
2019-04-12 12:37:18 +0500

    🚚 Rename frequency.go to parallel_letter_frequency.go

--- exercism/parallel-letter-frequency/parallel_letter_frequency_test.go
+++ exercism/parallel-letter-frequency/parallel_letter_frequency_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// In the separate file frequency.go, you are given a function, Frequency(),
+// In the separate file parallel_letter_frequency.go, you are given a function, Frequency(),
 // to sequentially count letter frequencies in a single text.
 //
 // Perform this exercise on parallelism using Go concurrency features.
