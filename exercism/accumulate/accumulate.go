8df3d2a6cac2671d226fde3b629d23f89441d410
Dmitri Y. Lapshin <spacehug.o0@gmail.com>
2019-04-22 17:33:19 +0500

    ⚗️ Accumulate

---
+++ exercism/accumulate/accumulate.go
@@ -0,0 +1,7 @@
+package accumulate
+
+func Accumulate(inp []string, con func(string) string) []string {
+	var res = make([]string, len(inp))
+	for i, f := range inp {res[i] = con(f)}
+	return res
+}
