ea0619c1618d720c4ec424b835bf78c221943be6
Dmitri Y. Lapshin <spacehug.o0@gmail.com>
2019-05-06 17:32:15 +0500

    ⚗️ Crypto-square

---
+++ exercism/crypto-square/crypto_square.go
@@ -0,0 +1,42 @@
+package cryptosquare
+
+import (
+	"math"
+	"unicode"
+)
+
+func Encode(input string) string {
+	var norm []rune
+	for _, r := range input {
+		if unicode.IsDigit(r) || unicode.IsLetter(r) {
+			norm = append(norm, unicode.ToLower(r))
+		}
+	}
+	r, c := sizeRowCol(len(norm))
+	if c < 1 {return ""}
+	ret := make([]rune, c * r + c - 1)
+	for i := 0; i < c; i++ {
+		for j := 0; j < r + 1; j++ {
+			ix := j * c + i
+			jx := i * r + j + i
+			if len(ret) <= jx {continue}
+			if len(norm) > ix {
+				ret[jx] = norm[ix]
+				continue
+			}
+			ret[jx] = ' '
+		}
+	}
+	return string(ret)
+}
+func sizeRowCol(n int) (int, int) {
+	col := int(math.Sqrt(float64(n)))
+	row := col
+	if col * row < n {
+		col++
+	}
+	if col * row < n {
+		row++
+	}
+	return row, col
+}
